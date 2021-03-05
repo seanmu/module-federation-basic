89f4741dcf6cfdfe6b6c309a342461b3ba40e8cf
Sean <Sean@SeandeMacBook-Pro.local>
2021-03-05 12:23:56 +0800

    init

---
+++ order/src/RestaurantApp.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent, Fragment } from "react";
+
+class App extends PureComponent {
+  componentDidMount() {}
+
+  render() {
+    return <h1>Order Frontend</h1>;
+  }
+}
+
+export default App;
